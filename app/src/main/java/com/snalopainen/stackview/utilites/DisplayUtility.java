2a73568655042c6e5970d76077255bee46342501
snajdan890525 <435063994@qq.com>
2016-12-27 17:38:16 +0800

    first commit

---
+++ app/src/main/java/com/snalopainen/stackview/utilites/DisplayUtility.java
@@ -0,0 +1,31 @@
+package com.snalopainen.stackview.utilites;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Point;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.WindowManager;
+
+/**
+ * Created by snajdan on 2016/12/27.
+ */
+
+public class DisplayUtility {
+    public static int getScreenWidth(Context context) {
+        Point size = new Point();
+        ((Activity) context).getWindowManager().getDefaultDisplay().getSize(size);
+        return size.x;
+    }
+
+    public static int dp2px(Context context, int dp) {
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        display.getMetrics(displaymetrics);
+
+        return (int) (dp * displaymetrics.density + 0.5f);
+    }
+
+}
